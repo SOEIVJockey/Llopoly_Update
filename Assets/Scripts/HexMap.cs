6bfbb72f4f408452e6c43b72cd87a548ee3c3cfc
rob ashton <ashtonr132@gmail.com>
2020-04-21 18:02:53 +0100

    Globalised cell

--- Assets/Scripts/HexMap.cs
+++ Assets/Scripts/HexMap.cs
@@ -6,6 +6,7 @@ public class HexMap : MonoBehaviour
 {
     private static char delimeter = ',';
     private static hexCell[,] map;
+    private static hexCell cell;
     private static UnityEngine.Object[] mats = new UnityEngine.Object[0], pMats = mats, prefabs = mats;
     private static string shader = "Standard";
     private static Transform parentMap;
@@ -13,20 +14,21 @@ public class HexMap : MonoBehaviour
     internal static void LoadMap(Transform t, string sysPath, string texturesAndPhysicsPath)//call this to load map
     {
         parentMap = t;
-        readMap(sysPath); //setup tiles      
+        readMap(sysPath); //setup cells (do not call global hexCell cell until this is done)     
         loadTexturesAndPhysicsPrefabs(texturesAndPhysicsPath);
-        foreach (hexCell cell in map) //apply mesh's
+        foreach (hexCell c in map) //apply mesh's
         {
+            cell = c;
             bool flat = cell.type == "F" || cell.type == "U";
             if (cell.type == "X")//impassable component settings
             {
                 cell.obj.GetComponent<CapsuleCollider>().height = 100;
                 cell.obj.GetComponent<CapsuleCollider>().radius = cell.outerRadius();
-                cell.obj.GetComponent<MeshFilter>().mesh = addHexMesh(cell, flat);
+                cell.obj.GetComponent<MeshFilter>().mesh = addHexMesh(flat);
             }
             else // all other tile component settings
             {
-                cell.obj.GetComponent<MeshFilter>().mesh = addHexMesh(cell, flat);
+                cell.obj.GetComponent<MeshFilter>().mesh = addHexMesh(flat);
                 cell.obj.GetComponent<MeshCollider>().sharedMesh = cell.obj.GetComponent<MeshFilter>().mesh;
                 if (mats.Length > int.Parse(cell.landType))
                 {
@@ -40,7 +42,7 @@ public class HexMap : MonoBehaviour
             cell.obj.transform.position = new Vector3(cell.x * cell.innerRadius * 2 + (cell.y % 2 == 0 ? cell.innerRadius : 0), 0, cell.y * 3 / 2f); //must be set after addmesh
             if (flat) //forceflat's need walls
             {
-                addWalls(cell);
+                addWalls();
             }
             if (cell.tObject != "N") //cell object spawn
             {
@@ -100,7 +102,7 @@ public class HexMap : MonoBehaviour
         }
     }
 
-    private static Mesh addHexMesh(hexCell cell, bool flat)
+    private static Mesh addHexMesh(bool flat)
     {
         Mesh mesh = new Mesh();
         float innerRadius = cell.innerRadius, outerRadius = cell.outerRadius();
@@ -108,7 +110,7 @@ public class HexMap : MonoBehaviour
         List<Vector2> uv = new List<Vector2>();
         List<int> triangles = new List<int>();
         Vector3 center = cell.obj.transform.localPosition;
-        Vector3[] corners = getCorners(cell);
+        Vector3[] corners = getCorners();
 
         for (int i = 0; i < 6; i++) //loop through triangles
         {
@@ -127,7 +129,7 @@ public class HexMap : MonoBehaviour
         {
             if (vertices[i] == cell.obj.transform.localPosition) //set centre point height
             {
-                float x = flat ? cell.height : (getHeight(cell, 1) + getHeight(cell, 2) + getHeight(cell, 3) + getHeight(cell, 4) + getHeight(cell, 5) + getHeight(cell, 6) + cell.height) / 7;
+                float x = flat ? cell.height : (getHeight(1) + getHeight(2) + getHeight(3) + getHeight(4) + getHeight(5) + getHeight(6) + cell.height) / 7;
                 vertices[i] = new Vector3(vertices[i].x, Math.Max(x, cell.height), vertices[i].y);
             }
         }
@@ -138,29 +140,29 @@ public class HexMap : MonoBehaviour
         mesh.RecalculateNormals(); //redraw
         return mesh;
     }
-    private static void addWalls(hexCell cell)
+    private static void addWalls()
     {
         List<Vector3> vertices = new List<Vector3>();
         List<Vector2> uv = new List<Vector2>();
         List<int> triangles = new List<int>();
         Mesh wallMesh = new Mesh();
-        Vector3[] corners = getCorners(cell);
+        Vector3[] corners = getCorners();
         for (int i = 0; i < 6; i++) //loop through hexagon sides
         {
             int startIndex = vertices.Count;
-            if (corners[i] != corners[i] + (heightOverPoint(cell, corners[i]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i].y)))) //if i am highest i am equal which is fine unless i am actually equal and my height is 0
+            if (corners[i] != corners[i] + (heightOverPoint(corners[i]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i].y)))) //if i am highest i am equal which is fine unless i am actually equal and my height is 0
             {
                 vertices.Add(corners[i]); //left
-                vertices.Add(corners[i] + (heightOverPoint(cell, corners[i]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i].y)))); //bottom left
+                vertices.Add(corners[i] + (heightOverPoint(corners[i]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i].y)))); //bottom left
                 vertices.Add(corners[1 + i]);//right            
-                vertices.Add(corners[1 + i] + (heightOverPoint(cell, corners[i + 1]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i + 1].y)))); //bottom right
+                vertices.Add(corners[1 + i] + (heightOverPoint(corners[i + 1]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i + 1].y)))); //bottom right
 
                 uv.Add(corners[i]); //left
-                uv.Add(corners[i] + (heightOverPoint(cell, corners[i]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i].y)))); //bottom left
+                uv.Add(corners[i] + (heightOverPoint(corners[i]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i].y)))); //bottom left
                 uv.Add(corners[i + 1]); //right
-                uv.Add(corners[i + 1] + (heightOverPoint(cell, corners[i + 1]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i + 1].y)))); //bottom right
+                uv.Add(corners[i + 1] + (heightOverPoint(corners[i + 1]) ? -(Vector3.up * cell.height) : (Vector3.up * (cell.height - corners[i + 1].y)))); //bottom right
 
-                if (heightOverPoint(cell, corners[0]))
+                if (heightOverPoint(corners[0]))
                 {
                     triangles.Add(startIndex + 3);
                     triangles.Add(startIndex + 2);
@@ -196,23 +198,23 @@ public class HexMap : MonoBehaviour
         x.transform.localPosition = Vector3.zero;
         x.tag = cell.type == "F" ? "Wall" : "noClimbWall";
     }
-    private static Vector3[] getCorners(hexCell cell) {
+    private static Vector3[] getCorners() {
         return new Vector3[] //hexagon coordinates from centre + maxheight of point neighbours + self
         {
-            new Vector3(0f, Math.Max(Math.Max(cell.height, getHeight(cell, 1)), getHeight(cell, 2)), cell.outerRadius()),
-            new Vector3(cell.innerRadius, Math.Max(Math.Max(cell.height, getHeight(cell, 2)), getHeight(cell, 3)), 0.5f * cell.outerRadius()),
-            new Vector3(cell.innerRadius, Math.Max(Math.Max(cell.height, getHeight(cell, 3)), getHeight(cell,4)), -0.5f * cell.outerRadius()),
-            new Vector3(0f, Math.Max(Math.Max(cell.height, getHeight(cell, 4)), getHeight(cell, 5)), -cell.outerRadius()),
-            new Vector3(-cell.innerRadius, Math.Max(Math.Max(cell.height, getHeight(cell, 5)), getHeight(cell, 6)), -0.5f * cell.outerRadius()),
-            new Vector3(-cell.innerRadius, Math.Max(Math.Max(cell.height, getHeight(cell, 6)), getHeight(cell, 1)), 0.5f * cell.outerRadius()),
-            new Vector3(0f, Math.Max(Math.Max(cell.height, getHeight(cell, 1)), getHeight(cell, 2)), cell.outerRadius())
+            new Vector3(0f, Math.Max(Math.Max(cell.height, getHeight(1)), getHeight(2)), cell.outerRadius()),
+            new Vector3(cell.innerRadius, Math.Max(Math.Max(cell.height, getHeight(2)), getHeight(3)), 0.5f * cell.outerRadius()),
+            new Vector3(cell.innerRadius, Math.Max(Math.Max(cell.height, getHeight(3)), getHeight(4)), -0.5f * cell.outerRadius()),
+            new Vector3(0f, Math.Max(Math.Max(cell.height, getHeight(4)), getHeight(5)), -cell.outerRadius()),
+            new Vector3(-cell.innerRadius, Math.Max(Math.Max(cell.height, getHeight(5)), getHeight(6)), -0.5f * cell.outerRadius()),
+            new Vector3(-cell.innerRadius, Math.Max(Math.Max(cell.height, getHeight(6)), getHeight(1)), 0.5f * cell.outerRadius()),
+            new Vector3(0f, Math.Max(Math.Max(cell.height, getHeight(1)), getHeight(2)), cell.outerRadius())
         };
     }
-    private static bool heightOverPoint(hexCell cell, Vector3 v)
+    private static bool heightOverPoint(Vector3 v)
     {
         return cell.height >= v.y;
     }
-    private static float getHeight(hexCell cell, int c = 0)
+    private static float getHeight(int c = 0)
     {
         bool xOverMax = cell.x + 1 > map.GetLength(0) - 1, xUnderMin = cell.x - 1 < 0, yOverMax = cell.y + 1 > map.GetLength(1) - 1, yUnderMin = cell.y - 1 < 0; //stops out of range exceptions
         if (cell.y % 2 != 0)
